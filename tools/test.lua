1ff272ce8e5f408f0c7fd488ea2ba76247f30bfa
ElvishJerricco <elvishjerricco@gmail.com>
2014-10-10 12:18:00 -0400

    Test

---
+++ tools/test.lua
@@ -0,0 +1,28 @@
+local argparse = grin.getPackageAPI("ElvishJerricco/Grin", "argparse")
+
+local parser = argparse.new()
+parser
+    :switch"d"
+parser
+    :switch"e"
+parser
+    :switch"f"
+parser
+    :argument"file"
+
+local options = parser:parse({}, ...)
+if not options then
+    return
+end
+
+assert(options.file, "Usage: test [options] [file]")
+
+if options.d then
+    grin.assert(fs.isDir(shell.resolve(options.file)), "Not a directory", 0)
+end
+if options.f then
+    grin.assert(fs.exists(shell.resolve(options.file)) and not fs.isDir(shell.resolve(options.file)), "Not a file", 0)
+end
+if options.e then
+    grin.assert(fs.exists(shell.resolve(options.file)), "Does not exist", 0)
+end
